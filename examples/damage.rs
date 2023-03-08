c48a35e9e1cbb6f31835c941976b91e6fca64536
domiryuu <dawnofmyrequiem@gmail.com>
2023-03-08 01:47:25 -0500

    more cleanup and ignored files

---
+++ examples/damage.rs
@@ -0,0 +1,37 @@
+//use dicey::Equation;
+
+struct Person {
+    health: i32,
+    damage: dicey::Equation,
+}
+
+impl Person {
+    fn new(health: i32, d: &str) -> Self {
+        let damage = dicey::Equation::new(d);
+        Person { health, damage }
+    }
+    fn attack(&self) -> i64 {
+        self.damage.roll()
+    }
+    // fn damage(mut self, d: i64) {
+    //     self.health -= d as i32;
+    // }
+}
+
+fn main() {
+    let person1 = Person::new(100, "2d20");
+    let person2 = Person::new(100, "3d10");
+    let mut turns = 0;
+    let mut health = person2.health;
+    while health > 0 {
+        turns += 1;
+        health -= person1.attack() as i32;
+    }
+    println!("It took {} turns for person 1 to kill person 2", turns);
+    health = person1.health;
+    while health > 0 {
+        turns += 1;
+        health -= person2.attack() as i32;
+    }
+    println!("It took {} turns for person 2 to kill person 1", turns);
+}
