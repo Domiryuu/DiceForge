8d5bd860db34ac3e55855884522f0c8e02bfdc03
domiryuu <dawnofmyrequiem@gmail.com>
2023-03-08 20:51:51 -0500

    fixed some doc stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
 //! Compiled dice notation for rapid evaluation.
 //!
-//! use Dicey::Equation;
-//! Dicey::Equation::new("3d6+2")
+//! ```rust
+//! use dice_forge::Equation;
+//! let attack = Equation::new("3d6+2");
+//! println!("You attack for {} damage", attack.roll());
+//! ```
 //!
 //! you can then roll() the Equation
 //! or you can check its high(), low() average() or range()
 //!
 //! this library takes into account order of operations and can handle
-//! '/', '*', '^', 'dice notation(d3)', '+', '-'
+//! '/', '*', '^', 'dice notation', '+', '-'
 //! as of yet has a verbos print. currently only gives the product of the evalutaion
+//! high() and low() check for lowest rolls or highest rolls not highest or lowest possable values of the equation as a whole
 
 pub mod equation;
 pub use equation::Equation;
